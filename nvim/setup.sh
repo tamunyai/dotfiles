4bd7fc779f2215ce2d3afc78dd90efd54ef20879
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-02-25 14:40:26 +0200

    add auto create sub-directories command

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -4,9 +4,11 @@ DOTFILES="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd)"
 source "$DOTFILES/install/utils.sh"
 
 src="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+dst="$HOME/.config/"
+mkdir -p "$dst"
 
 install_dependencies "git" "gcc" "g++" "python3-venv" "npm" "unzip"
 
 # sudo add-apt-repository --yes ppa:neovim-ppa/unstable
 install "neovim"
-link_file "$src" "$HOME/.config/"
+link_file "$src" "$dst"
