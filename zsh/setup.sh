8e4fbe5270558a8a288f96c6f8fcd6e201eb2977
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-02-25 14:42:03 +0200

    update setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -10,20 +10,20 @@ link_file "$src/rc" "$HOME/.zshrc"
 
 # Check if the current shell is Zsh
 if [ "$SHELL" != "$(which zsh)" ]; then
-	sudo chsh -s "$(which zsh)" "$USER"
+    sudo chsh -s "$(which zsh)" "$USER"
 fi
 
 # Create .env.sh file if it doesn't exist
 create_env_file() {
-	if [ -f "$HOME/.env.sh" ]; then
-		success "$HOME/.env.sh file already exists, skipping"
+    if [ -f "$HOME/.env.sh" ]; then
+        success "$HOME/.env.sh file already exists, skipping"
 
-	else
-		echo "export DOTFILES=$DOTFILES" >"$HOME/.env.sh"
-		echo "export ZSH=$DOTFILES/zsh" >>"$HOME/.env.sh"
+    else
+        echo "export DOTFILES='$DOTFILES'" >"$HOME/.env.sh"
+        echo "export ZSH='$DOTFILES/zsh'" >>"$HOME/.env.sh"
 
-		success 'created ~/.env.sh'
-	fi
+        success 'created ~/.env.sh'
+    fi
 }
 
 create_env_file
