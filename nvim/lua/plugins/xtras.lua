7e1d79734d7b270f622f9eb5d959a44c71ad6625
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-04-01 00:14:42 +0200

    feat: add new plugin codeium

--- nvim/lua/plugins/xtras.lua
+++ nvim/lua/plugins/xtras.lua
@@ -1,4 +1,24 @@
 return {
+	{
+		"nvim-cmp",
+		dependencies = {
+			{
+				"Exafunction/codeium.nvim",
+				cmd = "Codeium",
+				build = ":Codeium Auth",
+				opts = {},
+			},
+		},
+		opts = function(_, opts)
+			table.insert(opts.sources, 1, {
+				name = "codeium",
+				group_index = 1,
+				priority = 100,
+			})
+		end,
+	},
+
+	{
 		"echasnovski/mini.animate",
 		event = "VeryLazy",
 		opts = function()
@@ -31,4 +51,5 @@ return {
 				},
 			}
 		end,
+	},
 }
