4e2c2b7a4c99d5c0babdb49b849aff87becd7676
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-04-07 06:26:42 +0200

    feat: add treesj plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,11 @@
 return {
 	{
+		"Wansmer/treesj",
+		keys = { { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" } },
+		opts = { use_default_keymaps = false, max_join_length = 150 },
+	},
+
+	{
 		"nvim-pack/nvim-spectre",
 		build = false,
 		cmd = "Spectre",
