ace5bbc44a44c8b6f82d6cffc3283da8d78f4f08
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-09-05 19:20:13 +0200

    fix: remove nvim config

--- nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.keymap.set
-
--- Escape clears search highlight in insert and normal modes
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear search highlight" })
-
--- Smart navigation in visual and select modes using j and k
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
