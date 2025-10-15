76d0c6c29a076c1d9b187befce66e8b05acc6706
Thendo A Munyai <60216128+tamunyai@users.noreply.github.com>
2025-10-15 20:06:24 +0200

    perf: better git ssh setup

--- scripts/setup-git-ssh.sh
+++ scripts/setup-git-ssh.sh
@@ -12,35 +12,45 @@ source "${SCRIPT_DIR}/utils.sh"
 
 # --- MAIN --------------------------------------------------------------------
 
-echo ''
-
 info "checking for SSH key..."
 
-# Check if SSH key exists
+# check if SSH key exists
+# usage: ./setup-git-sh.sh <github_email>
 if [ ! -f ~/.ssh/id_ed25519 ]; then
-	user "enter your GitHub email address: "
 	github_email=$1
 
-	# TODO: prompt user to verify email before proceeding
+	if [ -z "$github_email" ]; then
+		user "Enter your GitHub email address: "
+		read -r github_email
+	fi
 
 	if [ -z "$github_email" ]; then
-		fail "email address is required."
+		fail "Email address is required."
 	fi
 
-	ssh-keygen -t ed25519 -C "$github_email" -N "" || error "Failed to generate SSH key"
-	info "SSH key not found. A new SSH key has been created."
+	info "Generating a new SSH key for $github_email..."
 
-	if copy_to_clipboard "$HOME/.ssh/id_ed25519.pub"; then
-		success "SSH key has been copied to the clipboard"
+	ssh-keygen -t ed25519 -C "$github_email" -N "" || fail "Failed to generate SSH key"
+	success "A new SSH key has been created."
 
-	else
-		user "you can manually copy the key from the following file: ~/.ssh/id_ed25519.pub"
-	fi
+	user "Please manually copy the public key below:"
+	echo ''
+	cat ~/.ssh/id_ed25519.pub
+	echo ''
 
-	success "now, go to https://github.com/settings/keys and add the key."
+	info "Now, go to https://github.com/settings/keys and add the key."
 
 else
-	success "SSH key already exists. No action required."
+	info "SSH key already exists. No action required."
+fi
+
+# test connection
+if [ -f ~/.ssh/id_ed25519 ] && user "Do you want to test SSH connection to GitHub now? (y/n): "; then
+	read -r answer
+
+	if [[ "$answer" =~ ^[Yy]$ ]]; then
+		ssh -T git@github.com
+	fi
 fi
 
 echo ''
